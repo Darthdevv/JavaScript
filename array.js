10283fbcb109a68cf44da58cc06d3af8dc7a781e
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-04-15 23:25:51 +0200

    👾 modified

--- array.js
+++ array.js
@@ -1,26 +1,27 @@
-const array = Array(10) // array size is 10
+// ? generate an array with 10 random numbers(Floats) from 0 => 10 
+const array = Array(10) 
     .fill()
-  .map(() => 10 * Math.random()); // numbers from 0-50 (exclusive)
+  .map(() => 10 * Math.random()); 
 
 console.log(array);
 
-const array1 = Array(10) // array size is 10
+
+// ? generate an array with 10 random numbers(Integers) from 0 => 10
+const array1 = Array(10) 
   .fill()
-  .map(() => Math.floor(10 * Math.random())); // numbers from 0-50 (exclusive)
+  .map(() => Math.floor(10 * Math.random())); 
 
 console.log(array1);
 
-const array2 = Array(10) // array size is 10
+
+// ? generate an array with 10 random numbers(Integers) from 1 => 10
+const array2 = Array(10) 
   .fill()
-  .map(() => Math.ceil(10 * Math.random())); // numbers from 0-50 (exclusive)
+  .map(() => Math.ceil(10 * Math.random())); 
 
 console.log(array2);
 
 const array0 = Array.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 console.log(array0)
-
-
-let app = [];
-console.log(app)
 
 
