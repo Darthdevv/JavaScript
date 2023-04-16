6d6332a67c276ae6914f691a6a3cb778161def2c
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-04-16 02:49:51 +0200

    modified

--- array.js
+++ array.js
@@ -25,3 +25,56 @@ const array0 = Array.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 console.log(array0)
 
 
+// ? generate an array with random 6 numbers(Integers) from 0 => 10 ES6 syntax
+const array3 = Array.from({ length: 6 }, () => Math.floor(10 * Math.random()))
+console.log(array3);
+
+
+// ? Write a JavaScript program to find all unique values in an given array of numbers
+
+function uniqueValues(arr) {
+  return new Set(arr)
+} 
+// * another solution
+
+const unique_Values = (arr) => [...new Set(arr)]
+
+console.log(uniqueValues([1, 1, 2, 3, 3, 3]));
+console.log(unique_Values([1, 1, 3, 3, 4, 4, 6, 6]));
+
+// ? Write a JavaScript function to merge two arrays and removes all duplicates elements.
+
+function mergeArays(arr1,arr2) {
+  let mergedArrays = arr1.concat(arr2)
+  return [...new Set(mergedArrays)]
+}
+
+console.log(mergeArays([1,2,3,"bangladish","egypt",true],[3,4,5,1,"egypt"]));
+
+// * another solution
+function arrayUnique(array) {
+  var a = array.concat();
+  for (var i = 0; i < a.length; ++i) {
+    for (var j = i + 1; j < a.length; ++j) {
+      if (a[i] === a[j]) a.splice(j--, 1);
+    }
+  }
+
+  return a;
+}
+
+var arry1 = ["Vijendra", "Singh"];
+var arry2 = ["Singh", "Shakya"];
+// Merges both arrays and gets unique items
+var arry3 = arrayUnique(arry1.concat(arry2));
+console.log(arry3);
+
+
+var a = [1, 2, 3], b = [101, 2, 1, 10]
+var c = a.concat(b)
+var d = c.filter((item, pos) => c.indexOf(item) === pos)
+
+console.log(d);
+
+var m = [undefined, true].filter((x) => x === true);
+console.log(m)
