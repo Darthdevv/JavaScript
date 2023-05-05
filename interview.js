0575ceb91fe64a029738d6451cc0fba7d9643c22
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-05-05 10:59:24 +0300

    🪶 modified

---
+++ interview.js
@@ -0,0 +1,4 @@
+// Parsing: Converting a string to a native object
+JSON.parse(text);
+// Stringification: converting a native object to a string so it can be transmitted across the network
+JSON.stringify(object);
