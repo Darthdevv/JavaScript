72b6135f70c4a1019925e3a20ff7f4c303d4c185
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-05-10 23:40:31 +0300

    modified

--- interview.js
+++ interview.js
@@ -3,3 +3,11 @@ JSON.parse(text);
 // Stringification: converting a native object to a string so it can be transmitted across the network
 JSON.stringify(object);
 
+console.log(1 +  "2" + "2"); //ans: 122
+console.log(1 +  +"2" + "2"); //ans: 32
+console.log(1 +  -"1" + "2"); //ans: 02
+console.log(+"1" +  "1" + "2"); //ans: 112
+console.log( "A" - "B" + "2"); //ans: NaN2
+console.log( "A" - "B" + 2); //ans: NaN
+// “10,11” == [[[[10]],11]] //10,11 == 10,11, and: true
+// "[object Object]" == {name: "test"} //ans true
