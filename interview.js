9474b01ceaf3f7bcc13a74251858611412362371
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-05-22 05:01:11 +0300

    modified

--- interview.js
+++ interview.js
@@ -1,29 +1,15 @@
-// Parsing: Converting a string to a native object
-JSON.parse(text);
-// Stringification: converting a native object to a string so it can be transmitted across the network
-JSON.stringify(object);
 
-console.log(1 +  "2" + "2"); //ans: 122
-console.log(1 +  +"2" + "2"); //ans: 32
-console.log(1 +  -"1" + "2"); //ans: 02
-console.log(+"1" +  "1" + "2"); //ans: 112
-console.log( "A" - "B" + "2"); //ans: NaN2
-console.log( "A" - "B" + 2); //ans: NaN
-// “10,11” == [[[[10]],11]] //10,11 == 10,11, and: true
-// "[object Object]" == {name: "test"} //ans true
 
-function foo(){
-	return
-	{ name: 2 }
+
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {
+  console.log(alphabet[i]);
 }
-foo()
- //return undefined
-
-function payTaxes() {
-	return 5;
+for (let j = 1; j < len; j++) {
+  console.log(alphabet[j]);
 }
-
-console.log(payTaxes());
-
-console.log('hello world');
-console.log('hello yousef');
+for (let k = 0; k < len; k++) {
+  console.log(alphabet[k]);
+}
