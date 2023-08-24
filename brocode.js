843e4d8f4fd17d1dde12e81192401c916f57fe9d
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-08-25 00:13:06 +0300

    new

--- brocode.js
+++ brocode.js
@@ -3,12 +3,14 @@ console.time();
 console.timeLog();
 document.getElementById('greeting').innerHTML = 'yousef';
 
-document.getElementById("submit").onclick(() => {
+document.getElementById("submit").onclick=() => {
   let vala = document.getElementById("a").value;
+  vala = Number(vala);
   let valb = document.getElementById("b").value;
-  let valc = document.getElementById("c").value;
+  valb = Number(valb);
+  valc = Math.sqrt(Math.pow(vala, 2) + Math.pow(valb, 2));
   console.log(vala);
   console.log(valb);
-  console.log(valc);
-})
+  console.log('the hypotenuse is ' + valc);
+}
 
