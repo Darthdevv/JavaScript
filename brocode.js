7dc724398f4a9daea5d63738f858acb1ae25f7bd
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-08-24 20:09:58 +0300

    new

--- brocode.js
+++ brocode.js
@@ -3,4 +3,12 @@ console.time();
 console.timeLog();
 document.getElementById('greeting').innerHTML = 'yousef';
 
-console.log(!"");
+document.getElementById("submit").onclick(() => {
+  let vala = document.getElementById("a").value;
+  let valb = document.getElementById("b").value;
+  let valc = document.getElementById("c").value;
+  console.log(vala);
+  console.log(valb);
+  console.log(valc);
+})
+
