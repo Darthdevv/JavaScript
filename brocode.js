1a111d4881aa968cc7c5a00d181bc9283ddc49c7
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-08-28 00:35:45 +0300

    new

--- brocode.js
+++ brocode.js
@@ -1,18 +1,33 @@
-console.log(1);
-console.time();
-console.timeLog();
-document.getElementById('greeting').innerHTML = 'yousef';
+// console.log(1);
+// console.time();
+// console.timeLog();
+// document.getElementById('greeting').innerHTML = 'yousef';
 
-document.getElementById("submit").onclick=() => {
-  let vala = document.getElementById("a").value;
-  vala = Number(vala);
-  let valb = document.getElementById("b").value;
-  valb = Number(valb);
-  valc = Math.sqrt(Math.pow(vala, 2) + Math.pow(valb, 2));
-  console.log(vala);
-  console.log(valb);
-  console.log('the hypotenuse is ' + valc);
-}
-let word = 'Yousef';
-let adjutedword = word.toLowerCase().trim();
-console.log(adjutedword);
+// document.getElementById("submit").onclick=() => {
+//   let vala = document.getElementById("a").value;
+//   vala = Number(vala);
+//   let valb = document.getElementById("b").value;
+//   valb = Number(valb);
+//   valc = Math.sqrt(Math.pow(vala, 2) + Math.pow(valb, 2));
+//   console.log(vala);
+//   console.log(valb);
+//   console.log('the hypotenuse is ' + valc);
+// }
+// let word = 'Yousef';
+// let adjutedword = word.toLowerCase().trim();
+// console.log(adjutedword);
+
+
+  function isPrime(n) {
+
+    if (n <= 1) return false;
+
+    for (let i = 2; i < n; i++){
+      if (n % i == 0) return false;
+    }
+    return true;
+  }
+
+
+isPrime(2) ? console.log("true") : console.log("false");
+  
