feed114d9fe94bd3fe7bc1e14ed39ef60fd23114
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-04 14:52:22 +0300

    new

--- brocode.js
+++ brocode.js
@@ -2,7 +2,12 @@ console.log('hello world');
 
 let myFirstName = 'yousef ';
 let myLastName = 'tarek';
-const fullName = myFirstName + myLastName;
-console.log(fullName);
+const FULLNAME = myFirstName + myLastName;
+const MYREALNAME = FULLNAME;
+console.log(FULLNAME);
+console.log(MYREALNAME);
+console.log(typeof null);
 
+dog = 'doberman';
+console.log(dog);
 
