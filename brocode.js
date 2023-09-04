d2400d69504b139f196dc872b90f703fc088c48b
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-04 16:20:13 +0300

    new

--- brocode.js
+++ brocode.js
@@ -11,3 +11,21 @@ console.log(typeof null);
 dog = 'doberman';
 console.log(dog);
 
+const yousefAge = new Date().getFullYear() - 1999;
+console.log(yousefAge);
+console.log(yousefAge,  'hello');
+
+function timeForMilkAndCookies(date) {
+
+  let day = date.getDate();
+  let month = date.getMonth();
+
+  if (day === 24 && month === 11) {
+    return true
+  } else {
+    return false
+  }
+  }
+let date = new Date('December 24, 1999');
+
+console.log(timeForMilkAndCookies(date));
