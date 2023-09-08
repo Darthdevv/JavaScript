028eb4c2f84104041b2429e68f4d90eca5129f7d
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-08 12:05:12 +0300

    new

--- brocode.js
+++ brocode.js
@@ -1,31 +1,33 @@
-console.log('hello world');
+const calcAverage = (num1,num2,num3) => (num1 + num2 + num3) / 3;
 
-let myFirstName = 'yousef ';
-let myLastName = 'tarek';
-const FULLNAME = myFirstName + myLastName;
-const MYREALNAME = FULLNAME;
-console.log(FULLNAME);
-console.log(MYREALNAME);
-console.log(typeof null);
+const dolphinsAvg1 = calcAverage(44, 23, 71);
+const dolphinsAvg2 = calcAverage(85, 54, 41);
+const koalasAvg1 = calcAverage(65, 54, 49);
+const koalasAvg2 = calcAverage(23, 34, 27);
+console.log(dolphinsAvg1, koalasAvg1);
+console.log(dolphinsAvg2, koalasAvg2);
 
-dog = 'doberman';
-console.log(dog);
-
-const yousefAge = new Date().getFullYear() - 1999;
-console.log(yousefAge);
-console.log(yousefAge,  'hello');
-
-function timeForMilkAndCookies(date) {
-
-  let day = date.getDate();
-  let month = date.getMonth();
-
-  if (day === 24 && month === 11) {
-    return true
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= (2 * avgKoalas)) {
+    return `Team Dolphins wins with ${avgDolphins} points`
+  } else if(avgKoalas >= 2 * avgDolphins) {
+    return `Team koalas wins with ${avgKoalas} points`;
   } else {
-    return false
+    return `None of the teams were able to win`
   }
-  }
-let date = new Date('December 24, 1999');
+}
 
-console.log(timeForMilkAndCookies(date));
+console.log(checkWinner(dolphinsAvg1, koalasAvg1));
+console.log(checkWinner(dolphinsAvg2, koalasAvg2));
+
+// we can call functions in arrays as function expressions they produce value and arrays accept them
+
+const calcAge = birthYear => 2024 - birthYear;
+const YEARS = [1990, 1967, 1966, 1985];
+const AGES = [calcAge(YEARS[0]), calcAge(YEARS[1]), calcAge(YEARS[YEARS.length - 1])];
+console.log(AGES);
+
+const SHOWS = ['the last of us', ' house', 'breaking bad'];
+const REMOVED = SHOWS.pop();
+console.log(SHOWS);
+console.log(REMOVED);
