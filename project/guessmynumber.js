c7fb9a3b9550ebf9d00645a078a595c2e3b7f478
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-14 19:12:37 +0300

    new

--- project/guessmynumber.js
+++ project/guessmynumber.js
@@ -1,35 +1,61 @@
 'use strict';
 
-let success = !true;
-if (success) {
-  document.body.style.backgroundColor = '#60b347';
-}
-
 const message = document.querySelector('.message');
 message.innerHTML = '🎉 Correct Number';
 
-const number = document.querySelector('.number').innerHTML = 2; 
-const score = document.querySelector('.score').innerHTML = 17;
+
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
+
 const guess = document.querySelector('.guess').value;
 const checkBtn = document.querySelectorAll('.btn')[1];
+const againBtn = document.querySelectorAll('.btn')[0];
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-const markBox = document.querySelector('.number').textContent = secretNumber;
+let markBox = document.querySelector('.number');
+
+let scoreCounter = 20;
+let HighScore = 0;
 
 checkBtn.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('.message');
     message.innerHTML = '⛔️ No Number';
-  } else if(guess === secretNumber) {
+  } else if (guess === secretNumber) {
     document.querySelector('.message');
-    message.innerHTML = '✅ Correct Number';
+    message.innerHTML = '🎉 Correct Number';
+    markBox = document.querySelector('.number').textContent = secretNumber;
+    document.body.style.backgroundColor = '#60b347';
+    HighScore = scoreCounter;
+    highScore.textContent = HighScore;
   } else if (guess > secretNumber) {
-    document.querySelector('.message');
-    message.innerHTML = ' High';
+    if (scoreCounter > 1) {
+        document.querySelector('.message');
+        message.innerHTML = '📈 Too High';
+        scoreCounter--;
+        score.textContent = scoreCounter;
+    } else {
+        document.querySelector('.message');
+        message.innerHTML = '💥 You lost the game!';
+        score.textContent = 0;
+      }
   } else if (guess < secretNumber) {
-    document.querySelector('.message');
-    message.innerHTML = '✅ Low';
+    if (scoreCounter > 1) {
+        document.querySelector('.message');
+        message.innerHTML = '📉 Too Low';
+        scoreCounter--;
+        score.textContent = scoreCounter;
+    } else {
+        document.querySelector('.message');
+        message.innerHTML = '💥 You lost the game!';
+        score.textContent = 0;
+    }
   }
   console.log(guess);
 });
 
+function reset() {
+  location.reload();
+}
+
+againBtn.addEventListener('click', reset);
