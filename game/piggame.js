a58de442ac24994e46c7f9c332a1af5c38035e5e
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-18 07:27:55 +0300

    implementing pig game functionality

--- game/piggame.js
+++ game/piggame.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const score1El = document.querySelector('#score--0').textContent = 0;
+const score2El = document.querySelector('#score--1').textContent = 0;
+const current1El = document.getElementById('current--0');
+const current2El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+let score = 0;
+
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const randomNum = Math.trunc(Math.random() * 6) + 1;
+  console.log(randomNum);
+  if (player1.classList.contains('player--active')) {
+    if (randomNum === 1) {
+      score = 0;
+      document.querySelector('#score--0').textContent = score;
+      current1El.textContent = score;
+    } else {
+      score += randomNum;
+      document.querySelector('#score--0').textContent = score;
+      current1El.textContent = score;
+    }
+  } else {
+    if (randomNum === 1) {
+      score = 0;
+      document.querySelector('#score--1').textContent = score;
+      current2El.textContent = score;
+    } else {
+      score += randomNum;
+      document.querySelector('#score--1').textContent = score;
+      current2El.textContent = score;
+    }
+  }
+  if (randomNum === 1) {
+    diceEl.classList.remove('hidden');
+    diceEl.setAttribute('src', '/game/dice-1.png')
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
+  } else if (randomNum === 2) {
+    diceEl.classList.remove('hidden');
+    diceEl.setAttribute('src', '/game/dice-2.png');
+  } else if (randomNum === 3) {
+    diceEl.classList.remove('hidden');
+    diceEl.setAttribute('src', '/game/dice-3.png');
+  } else if (randomNum === 4) {
+    diceEl.classList.remove('hidden');
+    diceEl.setAttribute('src', '/game/dice-4.png');
+  } else if (randomNum === 5) {
+    diceEl.classList.remove('hidden');
+    diceEl.setAttribute('src', '/game/dice-5.png');
+  } else {
+    diceEl.classList.remove('hidden');
+    diceEl.setAttribute('src', '/game/dice-6.png')
+  }
+})
