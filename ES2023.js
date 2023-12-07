8bf563b75a3cde2a35ed3384c143fd2a05f3d467
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-12-07 04:25:49 +0200

    new

--- ES2023.js
+++ ES2023.js
@@ -1,72 +1,11 @@
 'use strict';
 
-// function declaration 
+async function randomQuote() {
+  const response = await fetch('https://api.quotable.io/random');
+  const quote = await response.json();
 
-// function showMessage(name, text = 'no') {
-//   name = 'samy';
-//   console.log(name + ':' + text)
-// }
-
-// showMessage('yousef', 'yes');
-
-
-// function showCount(count) {
-//   console.log(count ?? 'unknown')
-// }
-
-// showCount(null);
-// showCount(undefined);
-// showCount('');
-// showCount(0);
-// showCount(!0);
-
-
-// function checkAge(age) {
-//   if (age >= 18) {
-//     return true;
-//   } else {
-//     return confirm('Do you have permission from your parents?');
-//   }
-// }
-
-// let age = prompt('enter your age!',18);
-
-// if (checkAge(age)) {
-//   alert('Access granted');
-// } else {
-//   alert('Access denied');
-// }
-
-
-function doNothing() {
-  
+  // Output the quote and author name
+  console.log(quote);
+  console.log(`- ${quote.author}`);
 }
-
-console.log(doNothing());
-
-function checkAge(age) {
-  return age >= 18 ?  true : confirm('Did parents allow you?');
-}
-
-function checkAge(age) {
-  return age >= 18 || confirm('Did parents allow you?');
-}
-
-console.log(checkAge(19));
-
-function min(a,b) {
-  return Math.min(a, b);
-}
-
-console.log(min(1, 2));
-console.log(min(-1, 1));
-console.log(min(1, 1));
-
-
-function pow(x, n) {
-  return x ** n;
-}
-
-console.log(pow(2, 2));
-console.log(pow(3, 2));
-console.log(pow(3, 3));
+randomQuote();
