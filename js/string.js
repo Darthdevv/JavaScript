335f0f95b3b55902a51782b656b810fb6f6d8f09
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-01-19 13:30:26 +0200

    🌩️ updated

--- js/string.js
+++ js/string.js
@@ -42,9 +42,9 @@ function resetCart() {
     console.log(`Cart Quantity is: ${cartQuantity}`);
 }
 
-const text = 'please visit Microsoft';
-const newText = text.replace('Microsoft', 'FaceBook');
-console.log(text);
+const textt = 'please visit Microsoft';
+const newText = textt.replace('Microsoft', 'FaceBook');
+console.log(textt);
 console.log(newText);
 
 
@@ -55,3 +55,76 @@ console.log(airline.indexOf('air'));
 console.log(airline.slice(4, 7));
 console.log(airline.slice(-4));
 console.log(airline)
+
+const fullName = 'Yousef El-gohary';
+const [firstName, lastName] = fullName.split(' ');
+console.log(['Mr', firstName, lastName].join(' '));
+const user = 'jessica ann smith davis';
+
+
+let capitalized = [];
+function capitaliseStr(str) {
+    let strs = str.split(' ');
+    // for (const s of strs) {
+    //     capitalized.push(s[0].toUpperCase()+ s.slice(1))
+    // }
+    for (let i = 0; i < strs.length; i++) {
+        // capitalized.push(strs[i][0].toUpperCase()+ strs[i].slice(1))
+        capitalized.push(strs[i].replace(strs[i][0],strs[i][0].toUpperCase()))
+    }
+    return capitalized.join(' ');
+}
+console.log(capitaliseStr(user));
+
+
+
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+// THIS TEST DATA (pasted to textarea)
+// underscore_case
+//  first_name
+// Some_Variable 
+//   calculate_AGE
+// delayed_departure
+
+// SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+
+// HINT 1: Remember which character defines a new line in the textarea 😉
+// HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+// HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+// HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+// Afterwards, test with your own test data!
+
+// GOOD LUCK 😀
+
+document.body.append(document
+    .createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+
+const Btn = document.querySelector('button');
+Btn.addEventListener('click', function convertVariables() {
+    const text = document.querySelector('textarea').value;
+    const Arr = Array.from(text.split('\n'));
+    console.log(Arr)
+    const trimmed = Arr.map(item => item.trim());
+    const req = trimmed.map(item => item.toLowerCase().split('_'))
+    let rows = '';
+    for (const [a, b] of req) {
+        for (let i = 0; i < 1; i++) {
+            rows += '✅';
+            console.log(a+b[0].toUpperCase()+b.slice(1)+rows);
+        }
+    }
+    console.log('hello')
+    console.log(trimmed)
+})
+
