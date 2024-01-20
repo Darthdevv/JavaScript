dba58a61790c22f39bdb292551591acf2b885786
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-01-20 17:48:17 +0200

    🌩️ updated

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,30 @@
 'use strict';
+
+
+const flight = 'LH123'
+const yousef = {
+  name: 'yousef elgohary',
+  passport: 247394
+}
+
+function checkIn(flightNum,passenger){
+  flightNum = 'LH333';
+  passenger.name = 'hamo elsayed'
+
+  passenger.passport === 247394 ? console.log('checked In') : console.log('wrong passport')
+}
+
+(checkIn(flight, yousef))
+console.log(flight, yousef);
+
+// functions returning other functions
+
+function greet(message) {
+  return function(name) {
+    console.log(`${message} ${name}`)
+  }
+}
+
+const greeter = greet('hello');
+greeter('yousef');
+greet('hello')('there !')
