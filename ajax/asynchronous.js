fe92ba8de7949d48ff53860ceedb9fb687b4c6ed
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-03-10 16:37:29 +0200

    🥑 first commit

---
+++ ajax/asynchronous.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const dogBreed = fs.readFileSync('./ajax/dog.txt', 'utf-8');
+console.log(dogBreed);
+
+fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
+  console.log(data);
+});
