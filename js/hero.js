819239dee169a5cc7833246cb6991e239479a1f2
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-03-12 22:03:43 +0200

    🥑 first commit

---
+++ js/hero.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const group = document.querySelector('.group');
+
+
+for (let i = 1; i < 20; i++) {
+  const container = document.createElement('div');
+  container.className = 'iconContainer';
+  container.id = `iconContainer${i}`;
+  group.appendChild(container);
+}
+
+
