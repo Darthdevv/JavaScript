7e474e828732d8d5490d0843e30ae6a90e8590d6
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-28 23:58:36 +0300

    🍇  first commit

--- card/card.js
+++ card/card.js
@@ -1,17 +1,12 @@
 "use strict";
 
-const handleOnMouseMove = e => {
-
-  const { currentTarget: target } = e;
-
-  const rect = target.getBoundingClientRect(),
+document.getElementById("cards").onmousemove = e => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
     x = e.clientX - rect.left,
     y = e.clientY - rect.top;
 
-  target.style.setProperty("--mouse-x", `${x}px`);
-  target.style.setProperty("--mouse-y", `${y}px`);
-}
-
-for (const card of document.querySelectorAll('.card')) {
-  card.onmousemove = e => handleOnMouseMove(e);
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
 }
